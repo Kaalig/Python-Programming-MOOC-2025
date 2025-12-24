e49253e2ff99d90c7959d8e3d9a0429e730a1e6a
Kaalig <116390704+Kaalig@users.noreply.github.com>
2025-12-25 00:11:17 +0100

    Create TImes five.py

---
+++ Part 1/TImes five.py
@@ -0,0 +1,5 @@
+# Write your solution herenu
+number = int(input("Please type in a number: "))
+multiplicator = 5
+total = number * multiplicator
+print(f"{number} times {multiplicator} is {total} ")
