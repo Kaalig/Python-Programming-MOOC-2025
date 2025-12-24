cd6aaf25517638a0c9a94a7a8c2041b30ee44102
Kaalig <116390704+Kaalig@users.noreply.github.com>
2025-12-25 00:14:06 +0100

    Create 15 - Name and age.py

---
+++ Part 1/15 - Name and age.py
@@ -0,0 +1,8 @@
+# Write your solution here
+
+name = str(input("What is your name? "))
+year = int(input("Which year were you born?" ))
+future_age = 2021 - year
+
+
+print(f"Hi {name}, you will be {future_age} years old at the end of the year 2021")
