4d2988466202eeed38d75c2eb7df6d7b82621f0e
Kaalig <116390704+Kaalig@users.noreply.github.com>
2025-12-24 23:37:16 +0100

    Add program to calculate minutes in a year

---
+++ Part 1/4 - Minutes in a year.py
@@ -0,0 +1,8 @@
+"""
+Question : 
+Please write a program which prints out the number of minutes in a year. Use Python code to perform the calculation, as in the previous code example.
+"""
+
+#Answer
+# Write your solution here
+print(365*1440)
