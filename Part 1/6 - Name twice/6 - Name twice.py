fa01394f0c2ad47602e9f25d10abe12a4f2b1320
Kaalig <116390704+Kaalig@users.noreply.github.com>
2025-12-24 23:40:47 +0100

    Create 6 - Name twice.py

---
+++ Part 1/6 - Name twice/6 - Name twice.py
@@ -0,0 +1,9 @@
+"""
+Question :
+Please write a program which asks for the user's name and then prints it twice, on two consecutive lines.
+"""
+
+#Answer :
+name = input("What is your name? ")
+print(name)
+print(name)
