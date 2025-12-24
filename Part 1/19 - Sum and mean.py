60ce100f36e087062de2a7910c391723842d5f89
Kaalig <116390704+Kaalig@users.noreply.github.com>
2025-12-25 00:25:33 +0100

    Create 19 - Sum and mean.py

---
+++ Part 1/19 - Sum and mean.py
@@ -0,0 +1,8 @@
+# Write your solution here
+nbr1 = int(input("Number 1: "))
+nbr2 = int(input("Number 2: "))
+nbr3 = int(input("Number 3: "))
+nbr4 = int(input("Number 4: "))
+sum = nbr1 + nbr2 + nbr3 + nbr4
+mean = sum / 4
+print(f"The sum of the numbers is {sum} and the mean is {mean}")
