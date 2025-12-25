d0f8731d4b6be72b3a26d8f9f75b0fa4f3d5881f
Kaalig <116390704+Kaalig@users.noreply.github.com>
2025-12-25 21:13:50 +0100

    Create 2 - Number of characters.py

---
+++ Part 2 /2 - Number of characters.py
@@ -0,0 +1,8 @@
+
+word = input("Please type in a word: ")
+length = len(word)
+if length == 1:
+    print(f"Thank you!")
+else:
+    print(f"There are {length} in the word {word}")
+    print(f"Thank you!")
