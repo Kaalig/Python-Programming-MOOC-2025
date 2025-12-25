b93b01debb324c995c7cc8a9d853261d3c3956dc
Kaalig <116390704+Kaalig@users.noreply.github.com>
2025-12-25 21:28:07 +0100

    Create 5 - Greater than or equal to.py

---
+++ Part 2 /5 - Greater than or equal to.py
@@ -0,0 +1,9 @@
+nbr1 = int(input("Please type in the first number: "))
+nbr2 = int(input("Please type in another number: "))
+
+if nbr1 > nbr2:
+    print("The greater number was:", nbr1)
+elif nbr2 > nbr1:
+    print("The greater number was:", nbr2)
+else:
+    print("The numbers are equal!")
