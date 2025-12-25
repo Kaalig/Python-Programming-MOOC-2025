066e2c01e2e2d0b01a32947cf40ffeae4a052d71
Kaalig <116390704+Kaalig@users.noreply.github.com>
2025-12-25 01:12:23 +0100

    Create 26 - Calculator.py

---
+++ Part 1/26 - Calculator.py
@@ -0,0 +1,10 @@
+num1 = int(input("Number 1:"))
+num2 = int(input("Number 2:"))
+operator = input("Operation:")
+
+if operator == "add":
+    print(f"{num1} + {num2} = {num1 + num2}")
+if operator == "subtract":
+    print (f"{num1} - {num2} = {num1 - num2}")
+if operator == "multiply":
+    print(f"{num1} * {num2} = {num1 * num2}")
