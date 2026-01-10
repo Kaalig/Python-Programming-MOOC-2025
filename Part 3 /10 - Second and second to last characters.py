df5115cc1808386652466c121ef717e59a92295b
Kaalig <116390704+Kaalig@users.noreply.github.com>
2026-01-10 23:08:03 +0100

    Create 10 - Second and second to last characters.py

---
+++ Part 3 /10 - Second and second to last characters.py
@@ -0,0 +1,7 @@
+string = input("Please type in a string: ")
+i = len(string)
+
+if string[1] == string[-2]:
+    print("The second and the second to last characters are " + string[1])
+else:
+    print("The second and the second to last characters are different")
