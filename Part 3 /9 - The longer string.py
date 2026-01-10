b38e0af0637c16171082ed3a9337a0d99098fa7f
Kaalig <116390704+Kaalig@users.noreply.github.com>
2026-01-10 22:51:42 +0100

    Create 9 - The longer string.py

---
+++ Part 3 /9 - The longer string.py
@@ -0,0 +1,10 @@
+string_1 = str(input("Please type in a string 1: "))
+string_2 = str(input("Please type in a string 2: "))
+
+if len(string_1) > len(string_2):
+    print(f"{string_1} is longer")
+
+elif len(string_2) > len(string_1):
+    print(f"{string_2} is longer")
+else:
+    print("The strings are equally long")
