e6a597a03360609e1896e9651d048bfc8048e817
Kaalig <116390704+Kaalig@users.noreply.github.com>
2026-01-10 22:16:21 +0100

    Create 4 - Powers of two.py

---
+++ Part 3 /4 - Powers of two.py
@@ -0,0 +1,11 @@
+limit = int(input("Upper limit: "))
+number = 1
+
+while number <= limit:
+    if number == 1:
+        number + 1
+        print(number)
+        number *= 2
+    else:
+        print(number)
+        number *= 2
