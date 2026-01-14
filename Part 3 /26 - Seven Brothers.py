17905cfcce7e8984558dd9dd4ddfa13751d16c1e
Kaalig <116390704+Kaalig@users.noreply.github.com>
2026-01-14 22:13:58 +0100

    Add function to print sorted names of seven brothers

---
+++ Part 3 /26 - Seven Brothers.py
@@ -0,0 +1,7 @@
+def seven_brothers():
+    names = ["Aapo", "Eero", "Juhani", "Lauri", "Simeoni","Timo", "Tuomas"]
+    names.sort
+    print("\n".join(names))
+
+if __name__ == "__main__":
+   seven_brothers()
