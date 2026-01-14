da551b61ffb9864001d72b0620ba121c9ac29f83
Kaalig <116390704+Kaalig@users.noreply.github.com>
2026-01-14 21:38:11 +0100

    Create 19 - Does it contain vowels.py

---
+++ Part 3 /19 - Does it contain vowels.py
@@ -0,0 +1,16 @@
+string = str(input("Please type in a string: "))
+
+while True:
+    if "a" in string:
+        print("a found")
+    else:
+        print("a not found")
+    if "e" in string:
+        print("e found")
+    else:
+        print("e not found")
+    if "o" in string:
+        print("o found")
+    else:
+        print("o not found")
+    break
